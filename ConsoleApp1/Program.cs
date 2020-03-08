a5e329658f54ed1f5189ecd1b5164c9fe8ed11a8
Bekzat2021 <bekzat.work@gmail.com>
2020-03-08 20:44:05 +0500

    Adding Book class in Library with reference

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookLibrary;
 
 namespace ConsoleApp1
 {
@@ -10,34 +11,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string answer="";
-            do
-            {
-                Console.WriteLine("Please enter the times you want roll a dice:");
-                int count = Convert.ToInt32(Console.ReadLine());
 
-                RollTheDice(count);
-
-                Console.WriteLine("Repeat or stop? Enter(exit/quit): ");
-                answer = Convert.ToString(Console.ReadLine());
-
-            } while (answer != "exit" && answer != "quit");
-            Console.ReadKey();
-        }
-
-        /// <summary>
-        /// Roll the dice and show the result
-        /// </summary>
-        /// <returns>void</returns>
-        static public void RollTheDice(int rollTimes)
-        {
-            Random random = new Random();
-
-            for (int i = 0; i < rollTimes; i++)
-            {
-                int aRandomNumber = random.Next(1, 6);
-                Console.WriteLine($"A random dice number: {aRandomNumber}.");
-            }            
         }
     }
 }
