530c49c4b211472b887f6669460da8b39ad50821
Bekzat2021 <bekzat.work@gmail.com>
2020-03-09 23:00:52 +0500

    Adding new branch for test Chapter 19 Page 154 Obj

---
+++ Color.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Color
+    {
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            this.Red = red;
+            this.Green = green;
+            this.Blue = blue;
+            this.Alpha = alpha;
+        }
+
+        public Color(byte red, byte green, byte blue)
+        {
+            this.Red = red;
+            this.Green = green;
+            this.Blue = blue;
+            this.Alpha = 255;
+        }
+
+        public byte Red { get; set; }
+
+        public byte Green { get; set; }
+
+        public byte Blue { get; set; }
+
+        public byte Alpha { get; set; }
+
+        public int GetGrayScale()
+        {
+            return (Red + Green + Blue) / 3;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ball firstBall = new Ball(15, new Color(125, 50, 60));
+
+            firstBall.Throw();
+            firstBall.Throw();
+            firstBall.Throw();
+            firstBall.Pop();
+
+            Ball secondBall = new Ball(12, new Color(55, 70, 99));
+            Color theSecondBallColor = secondBall.GetColor();
+
+            Console.WriteLine(theSecondBallColor.Alpha);
+            Console.WriteLine(theSecondBallColor.Red);
+            Console.WriteLine(theSecondBallColor.Green);
+            Console.WriteLine(theSecondBallColor.Blue);
+        }
+    }
+}
