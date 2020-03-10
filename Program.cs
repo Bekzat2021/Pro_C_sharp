e1bf16905abe3d49210776ecc6f96c5c9ae0c362
Bekzat2021 <bekzat.work@gmail.com>
2020-03-10 23:02:01 +0500

    Start making Tic-Tac-Toe project add Render method

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static int[] playFieldSquare = new int[9];
+
+        static void Main(string[] args)
+        {
+            RenderAPlayField(1);
+        }
+
+        static void RenderAPlayField(int sector)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(" "+playFieldSquare[i]);
+                if (i == 2)
+                {
+                    continue;
+                }
+                Console.Write(" |");
+            }
+            Console.WriteLine();
+            Console.WriteLine("---+---+---");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(" " + playFieldSquare[i]);
+                if (i == 2)
+                {
+                    continue;
+                }
+                Console.Write(" |");
+            }
+            Console.WriteLine();
+            Console.WriteLine("---+---+---");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(" " + playFieldSquare[i]);
+                if (i == 2)
+                {
+                    continue;
+                }
+                Console.Write(" |");
+            }
+            Console.WriteLine();
+        }
+    }
+}
