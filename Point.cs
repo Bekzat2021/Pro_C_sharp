c02bd978960697136f618f107cf26282adcb2c90
Bekzat2021 <bekzat.work@gmail.com>
2020-03-22 21:40:21 +0500

    Chapter 33 Page 241

---
+++ Point.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Point
+    {
+        private double x;
+        private double y;
+
+        public double X {
+            get {
+                return x;
+            }
+            set {
+                x = value;
+                OnEventChanged();
+            }
+        }
+
+        public double Y
+        {
+            get {
+                return y;
+            }
+            set {
+                y = value;
+                OnEventChanged();
+            }
+        }
+
+        public event EventHandler PointChanged;
+
+        public void OnEventChanged()
+        {
+            if (PointChanged!=null)
+            {
+                PointChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tic-Tac-Toe")]
+[assembly: AssemblyTitle("Events")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tic-Tac-Toe")]
+[assembly: AssemblyProduct("Events")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("86dd1bb8-a688-4f3b-bceb-35a5544b3c9b")]
+[assembly: Guid("a66f84a4-51e3-489c-85f1-7e943a905030")]
 
 // Version information for an assembly consists of the following four values:
 //
